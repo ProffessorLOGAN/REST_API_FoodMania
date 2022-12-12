4050a6e1124b1cd6cd8a0bb82453a1ad6dbe38cd
ProffessorLOGAN <82794943+ProffessorLOGAN@users.noreply.github.com>
2022-12-12 09:10:37 +0530

    Config and CustomErrorHandler Service created

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const { APP_PORT } = require('./config');
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('hello world')
-})
+});
 
+
+app.use(errorHandler);
+const PORT = process.env.PORT || APP_PORT;
 app.listen(3000, () => {
-    console.log('listening on port 3000');
-})
+    console.log(`listening on port ${PORT}`);
+});
