c3b7e321f770e030f77853ebf8eb7e2d7f3c2c19
ProffessorLOGAN <82794943+ProffessorLOGAN@users.noreply.github.com>
2022-12-14 23:54:14 +0530

    User Profile me authorized  with access token function created

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
 const router = express.Router();
 import { loginController, registerController } from '../controllers';
+import userController from '../controllers/auth/userController';
+import auth from '../middleware/auth';
 
 router.post('/register', registerController.register);
 router.post('/login', loginController.login);
+router.get('/me', auth, userController.me);
 
 export default router;
--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use('/api', routes);
-// app.use('/uploads', express.static('uploads'));
+app.use('/uploads', express.static('uploads'));
 app.get('/', (req, res) => {
     res.send('hello world')
 });
