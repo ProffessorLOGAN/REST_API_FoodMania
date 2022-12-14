db2134d5e999ec1c3c88254c4a90656dd5e776d5
ProffessorLOGAN <82794943+ProffessorLOGAN@users.noreply.github.com>
2022-12-15 00:06:03 +0530

    Refresh token verification function added

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 import express from 'express';
 const router = express.Router();
-import { loginController, registerController } from '../controllers';
-import userController from '../controllers/auth/userController';
+import { loginController, registerController,userController,refreshController } from '../controllers';
 import auth from '../middleware/auth';
 
 router.post('/register', registerController.register);
 router.post('/login', loginController.login);
 router.get('/me', auth, userController.me);
+router.post('/refresh', refreshController.refresh);
 
 export default router;
