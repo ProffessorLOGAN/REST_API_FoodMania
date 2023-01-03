f6ea055dcf7dd4105ba9075a425e17dc64a5247c
ProffessorLOGAN <82794943+ProffessorLOGAN@users.noreply.github.com>
2023-01-03 20:14:21 +0530

    Final product

--- config/index.js
+++ config/index.js
@@ -3,12 +3,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const {
-    APP_PORT=8000,
-    DEBUG_MODE=true,
-    APP_URL=`http://localhost:8000`,
-    DB_URL=`mongodb+srv://proffessor:logan@cluster0.adfkkil.mongodb.net/foodmania`,
-    authSource=`admin&w=1`,
-    REFRESH_SECRET=`secrettoken`,
-    JWT_SECRET=`thisappisawesome`,
-    ON_HEROKU=false,
+    APP_PORT,
+    DEBUG_MODE,
+    APP_URL,
+    DB_URL,
+    authSource,
+    REFRESH_SECRET,
+    JWT_SECRET,
+    ON_HEROKU,
 } = process.env;
