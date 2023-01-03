c87e72a5f820ecb56bb2a948dcac098873f756e8
ProffessorLOGAN <82794943+ProffessorLOGAN@users.noreply.github.com>
2023-01-03 20:09:31 +0530

    Host version@1.1

--- validators/productValidator.js
+++ validators/productValidator.js
@@ -4,7 +4,7 @@ const productSchema = Joi.object({
     name: Joi.string().required(),
     price: Joi.number().required(),
     category: Joi.string().required(),
-    image: Joi.string().required(),
+    image: Joi.string(),
 });
 
 export default productSchema;
